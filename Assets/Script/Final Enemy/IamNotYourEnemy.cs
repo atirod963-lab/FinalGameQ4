60adc9e3f1abd9dd8c1fcb004247d2218841ae6d
kong1735 <kk.game1735@gmail.com>
2025-11-11 20:19:30 +0700

    Enemy เลือดลดแล้วเข้ามาตีเรา

---
+++ Assets/Script/Final Enemy/IamNotYourEnemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class IamNotYourEnemy : Enemy
+{
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (player == null)
+        {
+            animator.SetBool("Attack", false);
+            return;
+        }
+
+        Turn(player.transform.position - transform.position);
+        timer -= Time.deltaTime;
+
+        if (health < maxHealth)
+        {
+            animator.SetBool("Attack", false);
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+            Move(direction); // เดินเข้าหาเสมอเมื่อเลือดลด
+
+            if (GetDistanPlayer() < 1.5)
+            {
+                Attack(player); // โจมตีเมื่อใกล้
+            }
+        }
+        else
+        {
+            animator.SetBool("Attack", false);
+        }
+    }
+
+}
